d24b319bedfc01bdd17e4d82ed62ee4319fed457
rhonegarcia1-crypto <rhonegarcia1@gmail.com>
2026-03-05 15:06:39 +0800

    Update AuthLoginForm.jsx to use a static blue gradient background

---
+++ Vesta/src/pages/AuthLoginForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AuthLoginForm.css';
+
+const AuthLoginForm = () => {
+    return (
+        <div className="auth-login-form" style={{ background: 'linear-gradient(to right, #0000FF, #00BFFF)' }}>
+            {/* Keep other form elements here */}
+            <h1>Login</h1>
+            {/* Other components remain unchanged */}
+        </div>
+    );
+};
+
+export default AuthLoginForm;
